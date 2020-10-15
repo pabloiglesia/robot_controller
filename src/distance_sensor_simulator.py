ea8dbea462486fac024f45eb28998b4385254602
pablo <pabloiglesia@outlook.com>
2020-10-15 10:24:14 +0200

    creation of Robot class

--- src/distance_sensor_simulator.py
+++ src/distance_sensor_simulator.py
@@ -2,11 +2,8 @@
 # license removed for brevity
 import rospy
 from std_msgs.msg import Float32
-from std_msgs.msg import Bool
 
 from ur_icam_description.robotUR import RobotUR
-
-import random
 
 # Global variable to calibrate distance between the robot and the table
 PICK_MOVEMENT_DISTANCE = 0.215
@@ -14,22 +11,15 @@ PICK_MOVEMENT_DISTANCE = 0.215
 # Global variable for myRobot
 MY_ROBOT = RobotUR()
 
-
 def talker():
-    distance_pub = rospy.Publisher('/distance', Float32)
-    gripper_pub = rospy.Publisher('/object_gripped', Bool)
-    rospy.init_node('/arduino', anonymous=True)
+    pub = rospy.Publisher('/distance', Float32)
+    rospy.init_node('distance_sensor', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         distance = MY_ROBOT.get_current_pose().pose.position.z - PICK_MOVEMENT_DISTANCE
         rospy.loginfo("Measure distance: {}".format(distance))
-        distance_pub.publish(distance)
-
-        object_gripped = random.random() > 0.7
-        rospy.loginfo("Object_gripped: {}".format(object_gripped))
-        gripper_pub.publish(object_gripped)
+        pub.publish(distance)
         rate.sleep()
-
 
 if __name__ == '__main__':
     try:
