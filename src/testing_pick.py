53a7dd3ccbf1a745fe0e3caf0d2d7cb255eee5dc
pablo <pabloiglesia@outlook.com>
2020-11-23 09:20:19 +0100

    Creating testing_pick.py

--- src/testing_pick.py
+++ src/testing_pick.py
@@ -5,27 +5,27 @@ from Robot import Robot
 from ai_manager.Environment import Environment
 from std_msgs.msg import Bool
 
+if __name__ == '__main__':
 
+    robot = Robot()
 
-robot = Robot()
+    while True:
+        waypoints = []
+        wpose = robot.robot.get_current_pose().pose
+        wpose.position.z -= (wpose.position.z - 0.24)  # Third move sideways (z)
+        waypoints.append(copy.deepcopy(wpose))
 
-while(True):
-    waypoints = []
-    wpose = robot.robot.get_current_pose().pose
-    wpose.position.z -= (wpose.position.z - 0.24)  # Third move sideways (z)
-    waypoints.append(copy.deepcopy(wpose))
-    
-    (plan, fraction) = robot.robot.move_group.compute_cartesian_path(
-        waypoints,  # waypoints to follow
-        0.01,  # eef_step
-        0.0)  # jump_threshold
-    robot.robot.move_group.execute(plan, wait=True)
-    
-    distance_ok = rospy.wait_for_message('distance', Bool).data  # We retrieve sensor distance
-    while distance_ok:
+        (plan, fraction) = robot.robot.move_group.compute_cartesian_path(
+            waypoints,  # waypoints to follow
+            0.01,  # eef_step
+            0.0)  # jump_threshold
+        robot.robot.move_group.execute(plan, wait=True)
+
         distance_ok = rospy.wait_for_message('distance', Bool).data  # We retrieve sensor distance
-    
-    robot.robot.move_group.stop()
-    
-    distance = Environment.CARTESIAN_CENTER[2] - robot.robot.get_current_pose().pose.position.z
-    robot.relative_move(0,0,distance)
+        while distance_ok:
+            distance_ok = rospy.wait_for_message('distance', Bool).data  # We retrieve sensor distance
+
+        robot.robot.move_group.stop()
+
+        distance = Environment.CARTESIAN_CENTER[2] - robot.robot.get_current_pose().pose.position.z
+        robot.relative_move(0, 0, distance)
